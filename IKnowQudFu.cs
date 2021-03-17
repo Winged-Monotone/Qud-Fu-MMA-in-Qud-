0571b6ec4238bb375e3719dc76c588c858f5dfba
Wingytone <lawrence.grant0@gmail.com>
2021-03-17 12:14:19 -0700

    Finally Fixed Wish, it Works

--- IKnowQudFu.cs
+++ IKnowQudFu.cs
@@ -1,69 +1,74 @@
-// // XRL.World.Capabilities.Wishing
-// using ConsoleLib.Console;
-// using Genkit;
-// using HistoryKit;
-// using Qud.API;
-// using Sheeter;
-// using System;
-// using System.Collections.Generic;
-// using System.Diagnostics;
-// using System.IO;
-// using System.Linq;
-// using System.Reflection;
-// using System.Text;
-// using UnityEngine;
-// using XRL.Annals;
-// using XRL.Core;
-// using XRL.Language;
-// using XRL.Messages;
-// using XRL.Rules;
-// using XRL.UI;
-// using XRL.Wish;
-// using XRL.World.AI.GoalHandlers;
-// using XRL.World.Effects;
-// using XRL.World.Parts;
-// using XRL.World.Parts.Mutation;
-// using XRL.World.Parts.Skill;
-// using XRL.World.QuestManagers;
-// using XRL.World.Skills.Cooking;
-// using XRL.World.Tinkering;
-// using XRL.World.ZoneBuilders;
+// XRL.World.Capabilities.Wishing
+using ConsoleLib.Console;
+using Genkit;
+using HistoryKit;
+using Qud.API;
+using Sheeter;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using XRL.Annals;
+using XRL.Core;
+using XRL.Language;
+using XRL.Messages;
+using XRL.Rules;
+using XRL.UI;
+using XRL.Wish;
+using XRL.World.AI.GoalHandlers;
+using XRL.World.Effects;
+using XRL.World.Parts;
+using XRL.World.Parts.Mutation;
+using XRL.World.Parts.Skill;
+using XRL.World.QuestManagers;
+using XRL.World.Skills.Cooking;
+using XRL.World.Tinkering;
+using XRL.World.ZoneBuilders;
 
 
-// using static XRL.World.IComponent<XRL.World.IPart>;
+using static XRL.World.IComponent<XRL.World.IPart>;
 
 
 
-// namespace MMAWishes
-// {
-//     [HasWishCommand]
-//     class MMAWishHandler
-//     {
-//         [WishCommand(Command = "IKnowQudFu")]
-//         public static void MasterMartialArts()
-//         {
-//             AddPlayerMessage("Show me.");
+namespace MMAWishes
+{
+    [HasWishCommand]
+    class MMAWishHandler
+    {
+        private static void AddSkill(BaseSkill Skill)
+        {
+            XRLCore.Core.Game.Player.Body.GetPart<XRL.World.Parts.Skills>().AddSkill(Skill);
+        }
 
-//             XRLCore.Core.Game.Player.Body.GetPart<XRL.World.Parts.Skills>().AddSkill("WM_MMA_SkillTree").;
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_SureStrikes");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_MasterStanceSwitch");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_MartialStances");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_MartialConI");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_MartialConII");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_MartialConIII");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_FlurryOfBlows");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_CombinationStrikesI");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_CombinationStrikesII");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_CombinationStrikesIII");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_PathSlumberling");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_PathSaltHopper");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_PathSaltBack");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_PathDawnGlider");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_PathAstralCabby");
-//             XRL.World.Capabilities.Wishing.AddSkill("WM_MMA_Grappler");
+        [WishCommand(Command = "IKnowQudFu")]
 
-//             XRL.World.Capabilities.Wishing.AwardXP(100000);
+        public static void MasterMartialArts()
+        {
 
-//         }
-//     }
-// }
+            AddPlayerMessage("Show me.");
+
+            AddSkill(new WM_MMASkillTree());
+            AddSkill(new WM_MMA_SureStrikes());
+            AddSkill(new WM_MMA_MasterStanceSwap());
+            AddSkill(new WM_MMA_MartialStances());
+            AddSkill(new WM_MMA_MartialConI());
+            AddSkill(new WM_MMA_MartialConII());
+            AddSkill(new WM_MMA_MartialConIII());
+            AddSkill(new WM_MMA_FlurryOfBlows());
+            AddSkill(new WM_MMA_CombinationStrikesI());
+            AddSkill(new WM_MMA_CombinationStrikesII());
+            AddSkill(new WM_MMA_CombinationStrikesIII());
+            AddSkill(new WM_MMA_PathSlumberling());
+            AddSkill(new WM_MMA_PathSalthopper());
+            AddSkill(new WM_MMA_PathSaltBack());
+            AddSkill(new WM_MMA_PathDawnGlider());
+            AddSkill(new WM_MMA_PathAstralTabby());
+            AddSkill(new WM_MMA_Grappler());
+
+        }
+    }
+}
